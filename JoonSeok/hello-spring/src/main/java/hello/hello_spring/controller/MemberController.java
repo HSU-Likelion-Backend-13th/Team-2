fabf2704e0befd728b0ce1ff80127713b8fdb033
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-03-22 16:37:27 +0900

    [섹션 6] 1. 회원 웹 기능 - 등록

--- JoonSeok/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ JoonSeok/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,9 +1,12 @@
 package hello.hello_spring.controller;
 
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +15,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new") // html이 /members/new에 post 요청하면 여기로 데이터가 꽂힌다.
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        // member 객체에 form 값을 집어넣어주는 장면이다.
+
+        memberService.join(member);
+        return "redirect:/";
     }
 }
