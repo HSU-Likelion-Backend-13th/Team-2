65f39de1bef2d7a1d7cd4d3c8ecdac649a732778
naeunmin <babaleo115@gmail.com>
2025-03-24 20:14:20 +0900

    [섹션7]6.스프링 데이터 JPA

--- EunMin/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ EunMin/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -12,6 +12,14 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 //    private DataSource dataSource;
 //
 //    @Autowired
@@ -19,23 +27,24 @@ public class SpringConfig {
 //        this.dataSource = dataSource;
 //    }
 
-    private EntityManager em;
+//    private EntityManager em;
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        // return new MemoryMemberRepository();
-        //return new JdbcMemberRepository(dataSource);
-        //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        // return new MemoryMemberRepository();
+//        //return new JdbcMemberRepository(dataSource);
+//        //return new JdbcTemplateMemberRepository(dataSource);
+//        // return new JpaMemberRepository(em);
+//
+//    }
 }
