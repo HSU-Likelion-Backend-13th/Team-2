13b13abf94eb014103713ee4f75dcb42a76c1c36
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-03-27 16:04:27 +0900

    [1주차 과제] 이준석 (#3)
    * [섹션 2] 4. View 환경설정
    
    * [섹션 3] 1. 정적 컨텐츠
    
    * [섹션 3] MVC와 템플릿 엔진
    
    * [섹션 3] API
    
    * [섹션 4] 회원 도메인과 리포지토리 만들기
    
    * FIX. findByName 수정
    
    * FIX. Id type String -> Long
    
    * [섹션 4] 회원 리포지토리 테스트 케이스 작성
    
    * [섹션 4] 4. 회원 서비스 개발
    
    * [섹션 4] 5. 회원 서비스 테스트
    
    * [섹션 5] 1. 컴포넌트 스캔과 자동 의존관계 설정
    
    * [섹션 5] 2. 자바 코드로 직접 스프링 빈 등록하기
    
    * [섹션 6] 1. 회원 웹 기능 - 홈 화면 추가
    
    * FIX. 회원가입 href 오탈자 수정.
    
    * [섹션 6] 1. 회원 웹 기능 - 등록
    
    * FIX. 잘못된 href 수정.
    
    * [섹션 6] 3. 회원 웹 기능 - 조회
    
    * [섹션 7] 2. 순수 JDBC
    
    * [섹션 7] 3. 스프링 통합 테스트]
    
    * [섹션 7] 2. 스프링 JdbcTemplate
    
    * [섹션 7] 5. JPA]
    
    * [섹션 7] 6. 스프링 데이터 JPA

---
+++ JoonSeok/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,47 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository; // Dependency Injection - 종속성 주입
+    }
+
+    /*
+     *  회원가입
+     */
+    public Long join(Member member) {
+        // 같은 이름의 중복 회원 제한
+        validateDuplicateMamber(member); // 중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMamber(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m ->{
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+    /*
+     * 전체 회원 조회 - service. So, business적 표현을 쓰는게 좋다 / repository는 개발자답게 써도 됨
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
