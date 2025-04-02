63f741435e8178e1657dac356d1b116710a649a1
zzuhannn <lunaaeric41@naver.com>
2025-04-02 16:39:06 +0900

    [2 주차] 2-(6) 스프링으로 전환

--- JuHan/week 2/core/src/main/java/hello/core/OrderApp.java
+++ JuHan/week 2/core/src/main/java/hello/core/OrderApp.java
@@ -7,15 +7,23 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 //        MemberService memberService = new MemberServiceImpl();
 //        OrderService orderService = new OrderServiceImpl();
+
+        // 이전에는 직접 객체를 생성해서 의존성 주입을 해줬지만,
+        // spring container를 통해서 해줄 수 있음
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId,"juhan", Grade.VIP );
