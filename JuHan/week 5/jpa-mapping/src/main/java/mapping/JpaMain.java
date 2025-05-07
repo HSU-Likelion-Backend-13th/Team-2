43841b331af23388ffef2e7438e0806944d3bf50
zzuhannn <lunaaeric41@naver.com>
2025-05-07 12:17:52 +0900

    ✨ Feat: 1.연관관계 매핑 코드 삭제

--- JuHan/week 5/jpa-mapping/src/main/java/mapping/JpaMain.java
+++ JuHan/week 5/jpa-mapping/src/main/java/mapping/JpaMain.java
@@ -5,7 +5,6 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
-import java.util.List;
 
 public class JpaMain {
 
@@ -16,34 +15,7 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("Team 1");
-            em.persist(team);
 
-            Member member = new Member();
-            member.setUsername("Member1");
-            // 위에 있는 팀을 받아줌
-            member.setTeam(team);
-            em.persist(member);
-
-            Member member2 = new Member();
-            member2.setUsername("Member2");
-            member2.setTeam(team);
-            em.persist(member2);
-
-            // 1차 캐시에 있는 데이터를 바로 DB로 보냄
-            em.flush();
-            // 남아있는 1차 캐시 데이터 정리
-            em.clear();
-
-            Member findMember = em.find(Member.class, member.getId());
-
-            List<Member> members = findMember.getTeam().getMembers();
-
-            for (Member m : members) {
-                System.out.println("멤버 = " + m.getUsername());
-                
-            }
 
             tx.commit();
         }catch (Exception e) {
