a723d6042d910059f499e796b24a89603a6947dd
zzuhannn <lunaaeric41@naver.com>
2025-05-07 14:02:37 +0900

    ✨ Feat: 연관관계 실습 코드 작성

--- JuHan/week 5/jpa-mapping/src/main/java/mapping/JpaMain.java
+++ JuHan/week 5/jpa-mapping/src/main/java/mapping/JpaMain.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+import java.time.LocalDateTime;
+
 
 public class JpaMain {
 
@@ -15,6 +17,29 @@ public class JpaMain {
         tx.begin();
 
         try {
+            // 멤버 생성
+            Member member = new Member("주한","성북구","한성대","공학관 코딩라운지");
+            em.persist(member);
+
+            // 아이템 생성
+            Item item = new Item("왕고구마" , 5000 , 5);
+            em.persist(item);
+
+            // OrderItem
+            OrderItem orderItem = new OrderItem();
+            orderItem.setCount(3);
+            orderItem.setOrderPrice(3*item.getPrice());
+            orderItem.setItem(item);
+            em.persist(orderItem);
+
+            // Order
+            Order order = new Order();
+            order.setMember(member);
+            order.setOrderDate(LocalDateTime.now());
+            order.setStatus(OrderStatus.SUCCESS);
+            order.addOrderItem(orderItem); //
+            em.persist(order);
+
 
 
             tx.commit();
